30481b2247e7621ec078db11e9658f8ab57fc3d7
ramakrishna <kesumallaramakrishna@gmail.com>
2023-04-15 00:51:43 +0530

    changes

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-069aabeee6f53e7bfls"
+  ami = "ami-069aabeee6f53e7bf"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
